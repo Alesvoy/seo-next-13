2e6b9f1cdb950ea7781e895f14bc68a7b1803301
wendy <25444364+wsparsons@users.noreply.github.com>
2023-04-03 08:31:26 -0700

    create generate static params

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,9 +1,19 @@
 export async function getProducts() {
-  const response = await fetch('https://fakestoreapi.com/products')
+  const response = await fetch("https://fakestoreapi.com/products");
 
   if (!response.ok) {
-    throw new Error("Failed to fetch products")
+    throw new Error("Failed to fetch products");
   }
 
-  return await response.json()
+  return await response.json();
+}
+
+export async function getProduct(id) {
+  const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch product");
+  }
+
+  return await response.json();
 }
--- src/app/products/[...slug]/page.jsx
+++ src/app/products/[...slug]/page.jsx
@@ -1,4 +1,5 @@
-import { getProducts } from '../api';
+import Image from "next/image";
+import { getProduct, getProducts } from "../../api/route";
 
 export async function generateStaticParams() {
   const products = await getProducts();
@@ -8,14 +9,22 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function ProductPage({ params }) {
-  const product = await getProducts(params.slug);
+export default async function ProductPage({ params: { slug } }) {
+  const product = await getProduct(slug);
 
   return (
     <div>
       <h1>{product.title}</h1>
+      <div>
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={500}
+          height={500}
+        />
+      </div>
       <p>{product.description}</p>
-      <p>{product.price}</p>
+      <p>${product.price}</p>
     </div>
   );
 }
